86f7cce71b94e5b281b606762a028c1bf4f17d96
Samin Gora <gorasamin6@gmail.com>
2026-03-15 21:33:05 +0545

    updating mobile view responsive

--- src/assets/components/footer/footer.jsx
+++ src/assets/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>copyrights &copy;2026 | Samin Gora.All right recived.</p>
+      <p>copyrights &copy;2026 | Samin Gora.All right reserved.</p>
     </div>
   );
 }
